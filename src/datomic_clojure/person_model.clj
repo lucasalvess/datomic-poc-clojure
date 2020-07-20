565e57531c1fa5fbd0756c221a368c376975c44e
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-07-20 20:08:11 -0300

    define person model

---
+++ src/datomic_clojure/person_model.clj
@@ -0,0 +1,13 @@
+(ns datomic-clojure.person-model)
+
+(defn create-person [cpf name gender email birth_date profession
+                    patrimony]
+  {
+   :person/cpf cpf
+   :person/name name
+   :person/gender gender
+   :person/email email
+   :person/birth_date birth_date
+   :person/profession profession
+   :person/patrimony patrimony
+   })
