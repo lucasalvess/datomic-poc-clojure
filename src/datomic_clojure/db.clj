8c9914bc3abcd3a94b9abef64c3e75924fac4b7e
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-07-20 19:55:44 -0300

    define database schema

--- src/datomic_clojure/db.clj
+++ src/datomic_clojure/db.clj
@@ -10,5 +10,49 @@
   (d/connect db-url))
 
 ;drop database if necessary
-(defn drop-database
+(defn drop-database []
   (d/delete-database db-url))
+
+;declare person schema
+(def schema [{
+              :db/ident       :person/cpf
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              :db/doc         "Document number"
+              :db/unique      :db.unique/identity
+              }
+             {
+              :db/ident       :person/name
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              }
+             {
+              :db/ident       :person/birth_date
+              :db/valueType   :db.type/instant
+              :db/cardinality :db.cardinality/one
+              }
+             {
+              :db/ident       :person/email
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              }
+             {
+              :db/ident       :person/gender
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              }
+             {
+              :db/ident       :person/patrimony
+              :db/valueType   :db.type/bigdec
+              :db/cardinality :db.cardinality/one
+              }
+             {
+              :db/ident       :person/profession
+              :db/valueType   :db.type/string
+              :db/cardinality :db.cardinality/one
+              }
+             ])
+
+;create schema
+(defn create-schema [conn]
+  (pprint (d/transact conn schema)))
