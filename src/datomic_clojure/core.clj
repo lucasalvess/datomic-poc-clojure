36d1bf71cf65dcf887a8baf5790e9f8615036ae7
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-07-20 20:13:45 -0300

    create person

---
+++ src/datomic_clojure/core.clj
@@ -0,0 +1,18 @@
+(ns datomic-clojure.core
+  (:use clojure.pprint)
+  (:require [datomic.api :as d]
+            [datomic-clojure.db :as db]
+            [datomic-clojure.person-model :as model]))
+
+;define an connection
+(def conn (db/open-connect))
+
+(db/create-schema conn)
+
+;define a map with the person model
+(let [person
+  (model/create-person "111.111.111-11", "Gandalf the Grey ", "M",
+                       "gandof@middleearth.com", (new java.util.Date), "Mage", 10000.00M)]
+
+;receive the connection and an sequence ([map])
+(pprint (d/transact conn [person])))
