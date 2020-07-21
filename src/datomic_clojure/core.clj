7f68df48e0bf5042a2426ed76c32880649d12524
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-07-21 12:10:12 -0300

    test update and delete in imutable database

--- src/datomic_clojure/core.clj
+++ src/datomic_clojure/core.clj
@@ -11,11 +11,11 @@
 
 ;define a map with the person model
 (let [person
-  (model/create-person "111.111.111-11", "Gandalf the Grey ", "M",
-                       "gandof@middleearth.com", (new java.util.Date), "Mage", 10000.00M)]
+      (model/create-person "111.111.111-11", "Gandalf the Grey ", "M",
+                           "gandof@middleearth.com", (new java.util.Date), "Mage", 10000.00M)]
 
-;receive the connection and an sequence ([map])
-(pprint (d/transact conn [person])))
+  ;receive the connection and an sequence ([map])
+  (pprint @(d/transact conn [person])))
 
 ; received the database state (SNAPSHOT)
 (def db (d/db conn))
@@ -24,3 +24,19 @@
 (pprint (d/q '[:find ?entity
                :where [?entity :person/name]] db))
 
+;return entity id and update and delete data
+(let [person2
+      (model/create-person "222.222.222-22", "Frodo Baggins", "M",
+                           "frodo@middleearth.com", (new java.util.Date), "Baggins", 700.00M)
+      result @(d/transact conn [person2])
+      id-entity (-> result :tempids vals first)]
+
+(pprint id-entity)
+
+;update patrimony
+(pprint @(d/transact conn [[:db/add id-entity :person/patrimony 0.1M]]))
+
+;remove
+(pprint @(d/transact conn [[:db/retract id-entity :person/profession "Baggins"]])))
+
+(db/drop-database)
