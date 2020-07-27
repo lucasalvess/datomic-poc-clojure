3bdadbb470d4778af8d45ac8b685730afcdff80a
Lucas Alves Da Silva <luca.silva@PPM-SPO-9042.local>
2020-07-27 18:57:40 -0300

    update dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
 (defproject datomic-clojure "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [com.datomic/datomic-free "0.9.5703.21"]]
-  :repl-options {:init-ns datomic-clojure.core})
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.datomic/datomic-free "0.9.5703.21"]
+                 [compojure "1.5.1"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [korma "0.3.0-RC5"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler datomic-clojure.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
